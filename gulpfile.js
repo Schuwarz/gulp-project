1326a66c1262d1f7cf7e48029060bcd23239175e
schuwarz <schuwarz@outlook.com>
2024-02-25 16:22:14 +0500

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const uglify = require('gulp-uglify')
 const concat = require('gulp-concat')
 const sourcemaps = require('gulp-sourcemaps')
 const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
 const del = require('del')
 
 const paths = {
@@ -17,6 +18,10 @@ const paths = {
   scripts: {
     src: 'src/scripts/**/*.js',
     dest: 'dist/js/'
+  },
+  images: {
+    src: 'src/img/*',
+    dest: 'dist/img'
   }
 }
 
@@ -54,14 +59,23 @@ function scripts() {
   .pipe(gulp.dest(paths.scripts.dest))
 }
 
+function img() {
+  return gulp.src(paths.images.src)
+  .pipe(imagemin({
+    progressive: true
+  }))
+  .pipe(gulp.dest(paths.images.dest))
+}
+
 function watch() {
   gulp.watch(paths.styles.src,styles)
   gulp.watch(paths.scripts.src,scripts)
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+const build = gulp.series(clean, gulp.parallel(styles, scripts, img), watch)
 
 exports.clean = clean
+exports.img = img
 exports.styles = styles
 exports.scripts = scripts
 exports.watch = watch
